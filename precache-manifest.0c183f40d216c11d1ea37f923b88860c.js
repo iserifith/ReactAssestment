ef5bf5becc0d4e2de0e1cb65aff55e782655ea20
seri <seriputera2@gmail.com>
2019-12-15 17:44:05 +0800

    Updates

---
+++ precache-manifest.0c183f40d216c11d1ea37f923b88860c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fd0e1cdd832bc159ab2a3766b62a7b",
+    "url": "/ReactAssestment/index.html"
+  },
+  {
+    "revision": "241da1702b6ea24b98ba",
+    "url": "/ReactAssestment/static/css/main.cd8c9c6b.chunk.css"
+  },
+  {
+    "revision": "f35f25134523bc0b9955",
+    "url": "/ReactAssestment/static/js/2.7f8d87dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactAssestment/static/js/2.7f8d87dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "241da1702b6ea24b98ba",
+    "url": "/ReactAssestment/static/js/main.0dacfcb0.chunk.js"
+  },
+  {
+    "revision": "53f0bbcd303bc58bf580",
+    "url": "/ReactAssestment/static/js/runtime-main.e243ba2f.js"
+  }
+]);
